e2545f290ee721c710992bfbf70d406782680afa
sandeeffendi <sande.effendi@gmail.com>
2026-02-13 01:50:32 +0700

    feat(auth): create options for swagger-jsdoc

---
+++ src/config/swagger.ts
@@ -0,0 +1,38 @@
+import {
+  default as swaggerJSDoc,
+  default as swaggerjsdoc,
+} from "swagger-jsdoc";
+
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+
+const swaggerOptions: swaggerjsdoc.Options = {
+  definition: {
+    openapi: "3.2.0",
+    info: {
+      title: "Todo List Simple CRUD API",
+      version: "1.0.0",
+      description: "API for Todo List Web Application",
+    },
+    servers: [
+      {
+        url: `http://${hostname}:${port}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export default swaggerSpec;
